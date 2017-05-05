af91657de36a79857069a03c65e109649321ce7d
Novak <a.novak@TSC-831.local>
2017-05-05 18:34:20 -0400

    +Blank macOS Cocoa Swift project

---
+++ HornDog/HornDog/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HornDog
+//
+//  Created by Novak, Alex on 5/5/17.
+//  Copyright © 2017 Novak, Alex. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
