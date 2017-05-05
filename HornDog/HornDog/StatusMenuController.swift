36a36c3b6398121019dfcb739ef5651c7e20e78b
Novak <a.novak@TSC-831.local>
2017-05-05 18:51:34 -0400

    +Blank StatusMenuController blank class

---
+++ HornDog/HornDog/StatusMenuController.swift
@@ -0,0 +1,13 @@
+//
+//  StatusMenuController.swift
+//  HornDog
+//
+//  Created by Novak, Alex on 5/5/17.
+//  Copyright © 2017 Novak, Alex. All rights reserved.
+//
+
+import Cocoa
+
+class StatusMenuController: NSObject {
+
+}
