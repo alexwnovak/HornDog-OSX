ad4acff6e9d7201aae1a2c7c6b28643d65cb2ab3
Novak <a.novak@TSC-831.local>
2017-05-05 18:58:04 -0400

    Moving the bootstrapping stuff into the new controller

--- HornDog/HornDog/AppDelegate.swift
+++ HornDog/HornDog/AppDelegate.swift
@@ -11,27 +11,10 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var statusMenu: NSMenu!
-    
-    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
-    
-    @IBAction func quitClicked(_ sender: NSMenuItem) {
-        NSApplication.shared().terminate(self)
-    }
-    
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        statusItem.menu = statusMenu
-        
-        let icon = NSImage(named: "statusIcon")
-        statusItem.image = icon
-        statusItem.menu = statusMenu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
-
-
 }
 
--- HornDog/HornDog/StatusMenuController.swift
+++ HornDog/HornDog/StatusMenuController.swift
@@ -9,5 +9,19 @@
 import Cocoa
 
 class StatusMenuController: NSObject {
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    
+    override func awakeFromNib() {
+        statusItem.menu = statusMenu
+        
+        let icon = NSImage(named: "statusIcon")
+        statusItem.image = icon
+        statusItem.menu = statusMenu
+    }
 
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared().terminate(self)
+    }
 }
